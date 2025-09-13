ca939c41803789d3a8a6686bd460229bfeb499b4
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-09-13 23:15:03 +0530

    Mysql Testing

---
+++ MySql_Connection/src/main/java/com/pasan/mysql_connection/MySqlConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.pasan.mysql_connection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySqlConnectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySqlConnectionApplication.class, args);
+    }
+
+}
