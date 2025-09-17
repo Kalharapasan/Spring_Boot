434b206fc04c71ecbee90bb173691d955328d6fb
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-09-18 00:59:07 +0530

    Create Prject pro crud operation

---
+++ CRUD-Operation/src/test/java/com/CRUD_Operation/CRUD/Operation/CrudOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CRUD_Operation.CRUD.Operation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
