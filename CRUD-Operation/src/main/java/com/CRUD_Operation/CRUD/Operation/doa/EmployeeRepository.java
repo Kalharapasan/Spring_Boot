fcad457659bdabd9a37151e838bcea854d415dca
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-09-19 19:10:19 +0530

    Create EmployeeCntroller Class and EmployeeRepository Interface File

---
+++ CRUD-Operation/src/main/java/com/CRUD_Operation/CRUD/Operation/doa/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.CRUD_Operation.CRUD.Operation.doa;
+
+import com.CRUD_Operation.CRUD.Operation.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository <Employee,Long>{
+
+
+
+}
--- CRUD-Operation/src/main/java/com/CRUD_Operation/CRUD/Operation/model/Employee.java
+++ CRUD-Operation/src/main/java/com/CRUD_Operation/CRUD/Operation/model/Employee.java
@@ -1,4 +1,38 @@
 package com.CRUD_Operation.CRUD.Operation.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table (name = "employee")
+
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="EMPLYEE_ID")
+    private  Long employeeId;
+
+    @Column(name="FIRST_NAME",length = 100)
+    private String firstName;
+
+    @Column(name="LAST_NAME",length = 100)
+    private String lastName;
+
+    @Column(name="EMAIL")
+    private String email;
+
+    @Column(name="AGE")
+    private  int age;
+
+    @Column(name="ADDRESS")
+    private  String address;
+
+
+
+
 }
