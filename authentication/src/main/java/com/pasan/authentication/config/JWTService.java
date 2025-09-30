d0808ca7b137d1ace1d6cac02840f710c6402279
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-09-30 13:31:34 +0530

    add  jjwt-api ,jjwt-impl and jjwt-jackson dependency for app

--- authentication/src/main/java/com/pasan/authentication/config/JWTService.java
+++ authentication/src/main/java/com/pasan/authentication/config/JWTService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JWTService {
-    public String extractUsername(String jwt) {
+    public String extractUsername(String token) {
+        return null;
     }
 }
