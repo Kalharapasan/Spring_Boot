034d3f89b46812afc8c6bed5b427a24e637da29d
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-09-30 23:36:33 +0530

    Complete and Test Authentication  demo project

--- authentication/src/main/java/com/pasan/authentication/config/JWTAuthenticationFilter.java
+++ authentication/src/main/java/com/pasan/authentication/config/JWTAuthenticationFilter.java
@@ -34,7 +34,7 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String username;
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+        if (authHeader == null || authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request,response);
             return;
         }
