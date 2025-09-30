042261f1ae2aa991c673870e083c485d623b7f27
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-09-30 23:06:20 +0530

    Create DemoController class in demo

--- authentication/src/main/java/com/pasan/authentication/auth/AuthenticationService.java
+++ authentication/src/main/java/com/pasan/authentication/auth/AuthenticationService.java
@@ -1,4 +1,48 @@
 package com.pasan.authentication.auth;
 
+import com.pasan.authentication.config.JWTService;
+import com.pasan.authentication.user.Role;
+import com.pasan.authentication.user.User;
+import com.pasan.authentication.user.UserRepositry;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+
 public class AuthenticationService {
+
+    private final UserRepositry userRepositry;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+       var user = User.builder()
+               .firstName(request.getFirstName())
+               .lastName(request.getLastName())
+               .email(request.getEmail())
+               .password(passwordEncoder.encode(request.getPassword()))
+               .role(Role.USER)
+               .build();
+       userRepositry.save(user);
+       var jwtToken = jwtService.generateToken(user);
+       return AuthenticationResponse.builder().token(jwtToken).build();
+
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepositry.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
 }
---
+++ authentication/src/main/java/com/pasan/authentication/demo/DemoController.java
@@ -0,0 +1,4 @@
+package com.pasan.authentication.demo;
+
+public class DemoController {
+}
