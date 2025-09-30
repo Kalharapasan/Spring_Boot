eefc7f2d51eec69d885b543e974e2207fbf7b1b8
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-09-30 13:26:29 +0530

    Create JWTService class in Config Pakege

---
+++ authentication/src/main/java/com/pasan/authentication/config/JWTService.java
@@ -0,0 +1,9 @@
+package com.pasan.authentication.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTService {
+    public String extractUsername(String jwt) {
+    }
+}
