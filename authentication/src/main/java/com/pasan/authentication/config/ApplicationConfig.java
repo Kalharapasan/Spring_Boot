ea5a062f9e0c8f6516338b6c9e60e0acde7529ac
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-09-30 22:20:28 +0530

    Create AuthenticationController class in auth

--- authentication/src/main/java/com/pasan/authentication/config/ApplicationConfig.java
+++ authentication/src/main/java/com/pasan/authentication/config/ApplicationConfig.java
@@ -4,7 +4,13 @@ import com.pasan.authentication.user.UserRepositry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.print.attribute.UnmodifiableSetException;
 
@@ -17,6 +23,24 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService() {
         return username -> userRepositry.findByEmail(username)
                 .orElseThrow(()->new UnmodifiableSetException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(PasswordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder PasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
